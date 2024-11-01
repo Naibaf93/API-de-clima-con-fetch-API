76b48867df27b6346dba0ee5fc3f43c3cee37e4b
Fabián Huertas <miloh9318@hotmail.com>
2024-11-01 14:36:47 -0500

    creando un evento submit y la funcion buscarClima

--- js/app.js
+++ js/app.js
@@ -1,3 +1,17 @@
 const container = document.querySelector('.container');
 const resultado = document.querySelector('#resultado');
 const formulario = document.querySelector('#formulario');
+
+window.addEventListener('load', () => {
+    formulario.addEventListener('submit', buscarClima);
+})
+
+function buscarClima(e) {
+    e.preventDefault();
+
+    // Validar
+    console.log('Buscando el clima...');
+
+    // Consultamos la API
+    
+}
